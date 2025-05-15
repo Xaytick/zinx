c463d2c6e95bb27b6dfe0da69c2e09b1c74d4a3d
Wang Xiang <xaywilliam0130@gmail.com>
2025-05-15 14:52:17 +0100

    修改心跳消息ID

--- utils/messageID.go
+++ utils/messageID.go
@@ -3,6 +3,6 @@ package utils
 // 系统预定义的消息ID常量
 const (
 	// 心跳相关
-	PING_MSG_ID uint32 = 100  // 心跳检测消息ID
-	PONG_MSG_ID uint32 = 101  // 心跳响应消息ID
+	PING_MSG_ID uint32 = 1  // 心跳检测消息ID
+	PONG_MSG_ID uint32 = 2  // 心跳响应消息ID
 ) 
